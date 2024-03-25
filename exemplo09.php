c5da14d447fe3ca001ed0ab3a87fb901edc368ba
LeoChristino <Leo.chrissantos28@gmail.com>
2024-03-25 20:00:24 -0300

    Calculadora IMC

---
+++ exemplo09.php
@@ -0,0 +1,10 @@
+<?php
+	$nome = "Leonardo";
+	$peso = 68;
+	$altura = 1.73;
+	function calcular ($peso,$altura){
+	return ($peso/($altura*$altura));
+}
+	$imc = calcular($peso,$altura,$altura);
+	printf("Olá ".$nome.", seu peso é ".$peso ."Kg, sua altura é " .$altura."m, e seu IMC é %.2f",$imc);
+ ?>
