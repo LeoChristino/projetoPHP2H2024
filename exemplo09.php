5c8f623be27ae7e0bff339d9e05429243bb9855b
LeoChristino <Leo.chrissantos28@gmail.com>
2024-03-25 20:11:05 -0300

    att

--- exemplo09.php
+++ exemplo09.php
@@ -5,6 +5,6 @@
 	function calcular ($peso,$altura){
 	return ($peso/($altura*$altura));
 }
-	$imc = calcular($peso,$altura,$altura);
+	$imc = calcular($peso,$altura);
 	printf("Olá ".$nome.", seu peso é ".$peso ."Kg, sua altura é " .$altura."m, e seu IMC é %.2f",$imc);
  ?>
